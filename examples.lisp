7a6801e8b54bd6dbeb3152f3642e35909af43894
Matthew Kennedy <burnsidemk@gmail.com>
2018-10-03 23:59:06 -0500

    watch out for shadowed list :S

--- examples.lisp
+++ examples.lisp
@@ -1,21 +1,14 @@
-(in-package #:iup)
+(in-package #:cl-user)
 
 (defun simple-notepad-3-1 ()
-  (with-iup
-    (let* ((text (print (multi-line :expand "YES")))
-	   (vbox (vbox (list text)))
-	   (dlg (dialog vbox :title "Simple Notepad" :size "QUARTERxQUARTER")))
-      (show-xy dlg +center+ +center+)
+  (iup:with-iup
+    (let* ((text (print (iup:multi-line :expand "YES")))
+	   (vbox (iup:vbox (list text)))
+	   (dlg (iup:dialog vbox :title "Simple Notepad" :size "QUARTERxQUARTER")))
+      (iup:show-xy dlg iup:+center+ iup:+center+)
 ;;      (setf (attr dlg :usersize) nil)
-      (main-loop))))
+      (iup:main-loop))))
 
-(defun simple-notepad-3-1 ()
-  (with-iup
-    (let* ((text (print (multi-line :expand "YES")))
-	   (dlg (dialog text :title "Simple Notepad" :size "QUARTERxQUARTER")))
-      (show-xy dlg +center+ +center+)
-;;      (setf (attr dlg :usersize) nil)
-      (main-loop))))
 
 #+nil
 (sb-int:with-float-traps-masked
@@ -24,50 +17,50 @@
 
 ;;; simple_notepad.c
 
-(defun create-main-dialog (config-handle)
-  ;; FIXME how to handle callbacks?
-  (let* ((multitext (multi-line :expand "YES" :name "MULTITEXT" :dirty "NO"))
-	 (lbl-statusbar (label :name "STATUSBAR" :expand "HORIZONTAL" :padding "10x5"))
-	 (item-new (item :title "&New	Ctrl+N" :image "IUP_FileNew"))
-	 (btn-new (button :image "IUP_FileNew" :flat "YES" :tip "New (Ctrl+N)" :canfocus "NO"))
-	 (item-open (item :title "&Open...	Ctrl+O" :image "IUP_FileOpen"))
-	 (btn-open (button :image "IUP_FileOpen" :flat "YES" :tip "Open (Ctrl+O)" :canfocus "NO"))
+;; (defun create-main-dialog (config-handle)
+;;   ;; FIXME how to handle callbacks?
+;;   (let* ((multitext (multi-line :expand "YES" :name "MULTITEXT" :dirty "NO"))
+;; 	 (lbl-statusbar (label :name "STATUSBAR" :expand "HORIZONTAL" :padding "10x5"))
+;; 	 (item-new (item :title "&New	Ctrl+N" :image "IUP_FileNew"))
+;; 	 (btn-new (button :image "IUP_FileNew" :flat "YES" :tip "New (Ctrl+N)" :canfocus "NO"))
+;; 	 (item-open (item :title "&Open...	Ctrl+O" :image "IUP_FileOpen"))
+;; 	 (btn-open (button :image "IUP_FileOpen" :flat "YES" :tip "Open (Ctrl+O)" :canfocus "NO"))
 
-	 (file-menu (menu (list item-new item-open)))
-	 (submenu-file (submenu file-menu :title "&File"))
-	 (menu (menu submenu-file))
+;; 	 (file-menu (menu (list item-new item-open)))
+;; 	 (submenu-file (submenu file-menu :title "&File"))
+;; 	 (menu (menu submenu-file))
 
-	 (toolbar-hb (hbox (list btn-new btn-open) :margin "5x5" :gap "2"))
-	 (vbox (vbox (list toolbar-hb multitext lbl-statusbar)))
-	 (dlg (dialog vbox :size "HALFxHALF" :menu menu)))
-    dlg))
+;; 	 (toolbar-hb (hbox (list btn-new btn-open) :margin "5x5" :gap "2"))
+;; 	 (vbox (vbox (list toolbar-hb multitext lbl-statusbar)))
+;; 	 (dlg (dialog vbox :size "HALFxHALF" :menu menu)))
+;;     dlg))
 
-(defun new-file (dialog-handle))
-(defun open-file (dialog-handle file))
+;; (defun new-file (dialog-handle))
+;; (defun open-file (dialog-handle file))
 
-(defun simple-notepad (&rest files)
-  (with-iup
-    (image-lib-open)
-    (let* ((config (config :app_name "simple_notepad")))
-      (config-load config)
-      (let* ((dialog (create-main-dialog config)))
-	(config-dialog-show config dialog "MainWindow")
-	(new-file dialog)
-	(dolist (file files)
-	  (open-file dialog file))
-	(main-loop)))))
+;; (defun simple-notepad (&rest files)
+;;   (with-iup
+;;     (image-lib-open)
+;;     (let* ((config (config :app_name "simple_notepad")))
+;;       (config-load config)
+;;       (let* ((dialog (create-main-dialog config)))
+;; 	(config-dialog-show config dialog "MainWindow")
+;; 	(new-file dialog)
+;; 	(dolist (file files)
+;; 	  (open-file dialog file))
+;; 	(main-loop)))))
 
-(defun simple-web-browser ()
-  (with-iup
-    (image-lib-open)
-    (iup-web:web-browser-open)
-    (let* ((web-browser (iup-web:web-browser))
-	   (vbox (vbox (list web-browser)))
-	   (dlg (dialog vbox :size "HALFxHALF")))
-      (show-xy dlg +center+ +center+)
-      (main-loop))))
+;; (defun simple-web-browser ()
+;;   (with-iup
+;;     (image-lib-open)
+;;     (iup-web:web-browser-open)
+;;     (let* ((web-browser (iup-web:web-browser))
+;; 	   (vbox (vbox (list web-browser)))
+;; 	   (dlg (dialog vbox :size "HALFxHALF")))
+;;       (show-xy dlg +center+ +center+)
+;;       (main-loop))))
 
-#+nil
-(sb-int:with-float-traps-masked
-    (:divide-by-zero)
-  (simple-web-browser))
+;; #+nil
+;; (sb-int:with-float-traps-masked
+;;     (:divide-by-zero)
+;;   (simple-web-browser))
