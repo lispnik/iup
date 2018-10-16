54ad6e5278a950eac1acd851b3422d5988bb6976
Matthew Kennedy <burnsidemk@gmail.com>
2018-10-16 15:35:00 -0500

    example

--- examples/glcontrols.lisp
+++ examples/glcontrols.lisp
@@ -69,13 +69,175 @@
   (repaint canvas)
   iup::+default+)
 
-(defun cube ()
+(defun glcontrols ()
   (iup:with-iup ()
     (iup-gl:open)
-    (setf canvas (iup-gl:canvas :rastersize "640x480" :buffer "DOUBLE" :depth_size "16"))
-    (let* ((dialog (iup:dialog
-		    canvas
-		    :title "IUP 3D OpenGL")))
+    (iup-glcontrols:open)
+    (iup-controls:open)
+    (let* ((glabel (iup-glcontrols:label :title "Label"
+					 :fgcolor "255 255 255"
+					 :font "Arial, 18"
+					 :image (load-image-tecgraf)))
+	   (gbutton1 (iup-glcontrols:button :title "Button"
+					    :padding "5x5"
+					    :action 'button-action-cb
+					    :name "button1"
+					    :tip "Button Tip"))
+	   (gbutton2 (iup-glcontrols:button :padding "5x5"
+					    :action 'button-action-cb
+					    :name "button2"
+					    :image (load-image-file-save)))
+	   (gtoggle (iup-glcontrols:toggle :title "Toggle"
+					   :padding "5x5"
+					   :action 'toggle-action-button-cb
+					   :name "toggle"))
+	   (gtoggle1 (iup-glcontrols:toggle :padding "5x5"
+					    :action 'toggle-action-button-cb
+					    :name "toggle"
+					    :image (load-image-test)))
+	   (gtoggle2 (iup-glcontrols:toggle :padding "5x5"
+					    :action 'toggle-action-button-cb
+					    :name "toggle"
+					    :image (load-image-test)))
+	   (gsep1 (iup-glcontrols:separator))
+	   (glink (iup-glcontrols:link :url "http://www.tecgraf.puc-rio.br/iup"
+				       :title "IUP Toolkit"
+				       :action 'link-action-cb))
+	   (pbar1 (iup-glcontrols:progress-bar :value 0.3 :show_text "YES"))
+	   (gval1 (iup-glcontrols:val :value 0.3
+				      :valuechanged_cb 'val-action-cb
+				      :progressbar pbar1
+				      :name "val1"
+				      :tip "Val Tip"
+				      :show_text "YES"))
+	   (hbox (iup:hbox (list glabel gsep1 gbutton1 gtoggle glink pbar1 gval1)
+			   :alignment "ACENTER"
+			   :margin "5x5"
+			   :gap "5"))
+	   (pbar2 (iup-glcontrols:progress-bar :value 0.3 :orientation "VERTICAL"))
+	   (gval2 (iup-glcontrols:val :value 0.3
+				      :orientation "VERTICAL"
+				      :valuechanged_cb 'val-action-cb
+				      :progressbar pbar2
+				      :name "val2"))
+	   (gsep2 (iup-glcontrols:separator))
+	   (vbox (iup:vbox (list gbutton2
+				 gsep2
+				 (iup:radio (iup:vbox (list gtoggle1 gtoggle2)) :margin "0x0")
+				 pbar2
+				 gval2)
+			   :alignment "ACENTER"
+			   :margin "5x5"
+			   :gap "5"))
+	   (gtoggle5 (iup-glcontrols:toggle :title "Toggle"
+					    :padding "5x5"
+					    :action 'toggle-action-cb
+					    :name "toggle5"
+					    :checkmark "YES"))
+	   (gtoggle3 (iup-glcontrols:toggle :title "Radio Toggle"
+					    :padding "5x5"
+					    :action 'toggle-action-cb
+					    :name "toggle3"
+					    :checkmark "YES"))
+	   (gtoggle4 (iup-glcontrols:toggle :title "Radio Toggle"
+					    :padding "5x5"
+					    :image (load-image-test)
+					    :action 'toggle-action-cb
+					    :name "toggle4"
+					    :checkmark "YES"))
+	   (vbox2 (iup:vbox (list (iup:radio (iup:vbox (list gtoggle3 gtoggle4)) :margin "0x0")
+				  gtoggle5)))
+	   (gsbox (iup-glcontrols:size-box (iup-glcontrols:scroll-box vbox2 :rastersize "90x90")))
+	   (gframe1 (iup-glcontrols:frame hbox :title "Frame1"))
+	   (gframe2 (iup-glcontrols:frame vbox :backcolor "250 250 160" :framecolor "250 250 160") )
+	   (gframe3 (iup-glcontrols:frame gsbox :title "Frame3"
+						:titlebox "YES"
+						:moveable "YES"
+						:position "550,200"))
+	   (gexp1 (iup-glcontrols:expander gframe1 :title "Expander"
+						   :horizontalalign "ACENTER"
+						   :verticalalign "ATOP"
+						   :moveable "YES"))
+	   (gexp2 (iup-glcontrols:expander gframe2 :barposition "LEFT"
+						   :horizontalalign "ALEFT"
+						   :verticalalign "ACENTER"))
+	   (text (iup:text :value "Text"))
+	   (matrix (iup-controls:matrix))
+;;   IupSetAttribute(matrix, "NUMLIN", "3");
+;;   IupSetAttribute(matrix, "NUMCOL", "2");
+;;   IupSetAttribute(matrix, "NUMLIN_VISIBLE", "3");
+;;   IupSetAttribute(matrix, "NUMCOL_VISIBLE", "2");
+;;   IupSetAttribute(matrix, "0:0", "Inflation");
+;;   IupSetAttribute(matrix, "1:0", "Medicine");
+;;   IupSetAttribute(matrix, "2:0", "Food");
+;;   IupSetAttribute(matrix, "3:0", "Energy");
+;;   IupSetAttribute(matrix, "0:1", "January 2000");
+;;   IupSetAttribute(matrix, "0:2", "February 2000");
+;;   IupSetAttribute(matrix, "1:1", "5.6");
+;;   IupSetAttribute(matrix, "2:1", "2.2");
+;;   IupSetAttribute(matrix, "3:1", "4.1");
+;;   IupSetAttribute(matrix, "1:2", "10");
+;;   IupSetAttribute(matrix, "2:2", "1");
+;;   IupSetAttribute(matrix, "3:2", "0.5");
+;; //  IupSetAttribute(matrix, "EXPAND", "No");
+;;   IupSetAttribute(matrix, "SCROLLBAR", "No");
+	   (vbox3 (iup-glcontrols:frame (iup:vbox (list text matrix)) :title "Frame4"
+								      :moveable "YES"
+								      :position "250,350"))
+	   (canvas (iup-glcontrols:canvas-box (list gexp1 gexp2 gframe3 vbox3) :depth_size 16))
+	   
+  ;; image_open = IupImage(16, 16, img_open);
+  ;; image_close = IupImage(16, 16, img_close);
+  ;; image_high = IupImage(16, 16, img_close);
+  ;; IupSetAttribute(image_open, "0", "BGCOLOR");
+  ;; IupSetAttribute(image_open, "1", "192 192 192");
+  ;; IupSetAttribute(image_close, "0", "BGCOLOR");
+  ;; IupSetAttribute(image_close, "1", "192 192 192");
+  ;; IupSetAttribute(image_high, "1", "192 192 192");
+
+  ;; //  IupSetAttribute(gexp1, "BARSIZE", "50");
+  ;; //  IupSetAttributeHandle(gexp1, "IMAGE", image_close);
+  ;; //  IupSetAttributeHandle(gexp1, "IMOPEN", image_open);
+  ;; //  IupSetAttribute(gexp1, "IMAGE", "img1");
+  ;; IupSetCallback(gexp1, "ACTION", (Icallback)expand_cb);
+  ;; IupSetAttribute(gexp1, "EXTRABUTTONS", "3");
+  ;; IupSetCallback(gexp1, "EXTRABUTTON_CB", (Icallback)extrabutton_cb);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRA1", image_close);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRAPRESS1", image_open);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRAHIGHLIGHT1", image_high);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRA2", image_close);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRAPRESS2", image_open);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRAHIGHLIGHT2", image_high);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRA3", image_close);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRAPRESS3", image_open);
+  ;; IupSetAttributeHandle(gexp1, "IMAGEEXTRAHIGHLIGHT3", image_high);
+  ;; //IupSetAttribute(gexp1, "REDRAWALL", "No");
+
+  ;; IupSetCallback(canvas, "ACTION", action);
+  ;; IupSetCallback(canvas, "BUTTON_CB", (Icallback)button_cb);
+  ;; IupSetCallback(canvas, "MOTION_CB", (Icallback)motion_cb);
+  ;; IupSetAttribute(canvas, "BUFFER", "DOUBLE");
+  ;; IupSetAttribute(canvas, "MARGIN", "10x10");
+
+  ;; box = IupVbox(canvas, NULL);
+  ;; IupSetAttribute(box, "MARGIN", "5x5");
+
+  ;; dlg = IupDialog(box);
+  ;; IupSetAttribute(dlg, "TITLE", "IupGLCanvas Test");
+  ;; IupSetAttribute(dlg, "RASTERSIZE", "800x600");
+
+  ;; IupMap(dlg);
+
+  ;; IupGLMakeCurrent(canvas);
+  ;; printf("Vendor: %s\n", glGetString(GL_VENDOR));
+  ;; printf("Renderer: %s\n", glGetString(GL_RENDERER));
+  ;; printf("Version: %s\n", glGetString(GL_VERSION));
+
+  ;; IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
+  ;; IupSetAttribute(dlg, "RASTERSIZE", NULL);
+	   (dialog (iup:dialog
+		    (iup:vbox (list (iup-glcontrols:canvas-box canvas :depth-size 16)))
+		    :title "IUPGLControls Test")))
       (setf (iup:callback canvas :action) 'repaint-cb
 	    (iup:callback canvas :resize_cb) 'resize-cb)
       (iup-cffi::%iup-set-function :idle_action (cffi:get-callback 'idle-cb))
@@ -85,4 +247,4 @@
 #+nil
 (sb-int:with-float-traps-masked
     (:divide-by-zero :invalid)
-  (cube))
+  (glcontrols))
