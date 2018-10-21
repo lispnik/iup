2b7c663e221e5c1a3426d32cb57b284a685dfe1c
Matthew Kennedy <burnsidemk@gmail.com>
2018-10-21 16:59:10 -0500

    support for nicer names

--- classesdb/classesdb.lisp
+++ classesdb/classesdb.lisp
@@ -2,24 +2,80 @@
 
 (defparameter *static-metadata*
   (list (list :initializer 'iup:open
-	      :child-p '("spinbox" "radio" "backgroundbox" "scrollbox" "flatscrollbox" "detachbox" "expander" "sbox")
-	      :children-p '("cbox" "gridbox" "hbox" "vbox" "zbox" "normalizer" "frame" "flatframe" "tabs" "flattabs" "split")
-	      :override-p '("image" "imagergb" "imagergba"))
-	(list :initializer 'iup-controls:open)
+	      :child-p '("submenu" "spinbox" "radio" "backgroundbox" "scrollbox" "flatscrollbox"
+			 "detachbox" "expander" "sbox")
+	      :children-p '("menu" "cbox" "gridbox" "hbox" "vbox" "zbox" "normalizer" "frame"
+			    "flatframe" "tabs" "flattabs" "split")
+	      :override-p '("image" "imagergb" "imagergba")
+	      :vanity-alist '(("gridbox" . "grid-box")
+			      ("flatframe" . "flat-frame")
+			      ("flattabs" . "flat-tabs")
+			      ("backgroundbox" . "background-box")
+			      ("scrollbox" . "scroll-box")
+			      ("flatscrollbox" . "flat-scroll-box")
+			      ("detachbox" . "detach-box")
+			      ("animatedlabel" . "animated-label")
+			      ("flatbutton" . "flat-button")
+			      ("dropbutton" . "drop-button")
+			      ("colorbrowser" . "color-browser")
+			      ("datepick" . "date-pick")
+			      ("flatlabel" . "flat-label")
+			      ("flatseparator" . "flat-separator")
+			      ("progressbar" . "progress-bar")
+			      ("flattoggle" . "flat-toggle")
+			      ("multiline" . "multi-line")
+			      ("submenu" . "sub-menu")
+			      ("colordlg" . "color-dialog")
+			      ("filedlg" . "file-dialog")
+			      ("fontdlg" . "font-dialog")
+			      ("messagedlg" . "message-dialog")
+			      ("progressdlg" . "progress-dialog")
+			      ("parambox" . "param-box")))
+	(list :initializer 'iup-controls:open
+	      :vanity-alist '(("matrixex" . "matrix-ex")
+			      ("matrixlist" . "matrix-list")))
 	(list :initializer 'iup-gl:open
-	      :child-p'("backgroundbox"))
+	      :child-p '("glbackgroundbox")
+	      :vanity-alist '(("glcanvas" . "canvas")
+			      ("glsubcanvas" . "sub-canvas")))
 	(list :initializer 'iup-glcontrols:open
 	      :child-p '("glexpander" "glframe" "glscrollbox" "glsizebox")
-	      :children-p '("glcanvasbox"))
-	(list :initializer 'iup-scintilla:open)
+	      :children-p '("glcanvasbox")
+	      :vanity-alist '(("glcanvasbox" . "canvas-box")
+			      ("glsubcanvas" . "sub-canvas")
+			      ("glprogressbar" . "progress-bar")
+			      ("glscrollbox" . "scroll-box")
+			      ("glsizebox" . "size-box")
+			      ("glval" . "val")
+			      ("gltoggle" . "toggle")	
+			      ("gltext" . "text")
+			      ("gllink" . "link")	
+			      ("glframe" . "frame")
+			      ("glexpander" . "expander")	
+			      ("glbutton" . "button")	
+			      ("gllabel" . "label")
+			      ("glseparator" . "separator")
+			      ("glbackgroundbox" . "background-box")))
 	(list :initializer 'iup-plot:open)
-	(list :initializer 'iup-mglplot:open)
+	(list :initializer 'iup-mglplot:open
+	      :vanity-alist '(("mglplot" . "plot")
+			      ("mgllabel" . "label")))
 	#+windows (list :initializer 'iup-olecontrol:open)
+	(list :initializer 'iup-scintilla:open
+	      :vanity-alist '(("scintilladlg" . "scintilla-dialog")))
 	(list :initializer 'iup-web:open
-	      :classname-excludes '("olecontrol")))
+	      :classname-excludes '("olecontrol")
+	      :vality-alist '(("webbrowser" . "web-browser"))))
   "Not everything about the IUP APIs can be extracted by
 introspection. This describes the static metadata that is augmented
-with IUP metadata upon introspection.")
+with IUP metadata upon introspection.
+
+VANITY-ALIST mapping from IUP classnames to nicer Lisp name
+INITIALIZER package qualified symbol for computing export package and initialization function
+CLASSNAME-EXCLUDES IUP classnames to exclude
+CHILDREN-P the IUP class can contain children
+CHILD-P the IUP class can contain a child
+OVERRIDE don't create this class automatically")
 
 (defun create-classesdb ()
   "Create a printable representaion of IUP metadata containing enough
@@ -39,16 +95,20 @@ information to create the Lisp API at compilation time."
 	  for children-p = (getf metadata :children-p)
 	  for child-p = (getf metadata :child-p)
 	  for override-p = (getf metadata :override-p)
+	  for vanity-alist = (getf metadata :vanity-alist)
 	  collect (iup:with-iup ()
 		    (funcall initializer)
 		    (list :package (package-name (symbol-package initializer))
-			  :classnames (loop for classname in difference
-					    collect (list :classname classname
-							  :child-p (and (find classname child-p :test #'string=) t)
-							  :children-p (and (find classname children-p :test #'string=) t)
-							  :override-p (and (find classname override-p :test #'string=) t)
-							  :attributes (sort-keywords (iup:class-attributes classname))
-							  :callbacks (sort-keywords (iup:class-callbacks classname))))))
+			  :classnames
+			  (loop for classname in difference
+				collect (list :classname classname
+					      :child-p (and (find classname child-p :test #'string=) t)
+					      :children-p (and (find classname children-p :test #'string=) t)
+					      :override-p (and (find classname override-p :test #'string=) t)
+					      :vanity-classname (if-let (vanity-name (assoc-value vanity-alist classname :test #'string=))
+								  (string-upcase vanity-name))
+					      :attributes (sort-keywords (iup:class-attributes classname))
+					      :callbacks (sort-keywords (iup:class-callbacks classname))))))
 	    into result
 	  finally (return (list* :platform (platform) :metadata result)))))
 
