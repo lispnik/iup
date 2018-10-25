5f048c703b23cb0576f76d83b13572de7f7550a3
Matthew Kennedy <burnsidemk@gmail.com>
2018-10-24 21:10:50 -0500

    meh

--- classesdb/classesdb.lisp
+++ classesdb/classesdb.lisp
@@ -1,172 +1,116 @@
 (in-package #:iup-classesdb)
 
-(iup:with-iup ()
-  (let* ((class (iup-classesdb-cffi.functions:iup-register-find-class "button"))
+(autowrap:define-bitmask-from-enum (iattrib-flags iup-classesdb-cffi:iattrib-flags))
+
+(defun attribute-metadata (class attrib table)
+  (cffi:with-foreign-objects
+      ((get-func :pointer)
+       (set-func :pointer)
+       (default-value :pointer)
+       (system-default :pointer)
+       (flags :int))
+    (iup-classesdb-cffi.functions:iup-class-register-get-attribute
+     class
+     attrib
+     get-func
+     set-func
+     default-value
+     system-default
+     flags)
+    (list
+     :type (iup-classesdb-cffi.functions:iup-table-get-curr-type table)
+     :get-func-p (not (cffi:null-pointer-p (cffi:mem-ref get-func :pointer)))
+     :set-func-p (not (cffi:null-pointer-p (cffi:mem-ref set-func :pointer)))
+     :default-value (cffi:foreign-string-to-lisp (cffi:mem-aref default-value :pointer))
+     :system-default (cffi:foreign-string-to-lisp (cffi:mem-aref system-default :pointer))
+     :flags (autowrap:mask-keywords 'iattrib-flags (cffi:mem-ref flags :int)))))
+
+(defun class-metadata (classname)
+  (let* ((class (iup-classesdb-cffi.functions:iup-register-find-class classname))
 	 (table (iup-classesdb-cffi.accessors:iclass-.attrib-func class)))
     (iup-classesdb-cffi.functions:iup-table-count table)
     (loop for attrib = (iup-classesdb-cffi.functions:iup-table-first table)
 	    then (iup-classesdb-cffi.functions:iup-table-next table)
 	  while attrib
-	  collect (cons attrib 
-			(cffi:with-foreign-objects
-			    ((get :pointer)
-			     (set :pointer)
-			     (default-value :pointer)
-			     (system-default :pointer)
-			     (flags :int))
-			  (iup-classesdb-cffi.functions:iup-class-register-get-attribute
-			   class
-			   attrib
-			   get
-			   set
-			   default-value
-			   system-default
-			   flags)
-			  (list
-			   :type (iup-classesdb-cffi.functions:iup-table-get-curr-type table)
-			   :get (cffi:mem-ref get :pointer)
-			   :set (cffi:mem-ref set :pointer)
-			   :default-value (cffi:foreign-string-to-lisp (cffi:mem-aref default-value :pointer))
-			   :system-default (cffi:foreign-string-to-lisp (cffi:mem-aref system-default :pointer))
-			   :flags (cffi:mem-ref flags :int)))))))
+	  collect (list* :name attrib (attribute-metadata class attrib table)))))
 
-;; iupClassRegisterGetAttribute (Iclass *ic, const char *name, IattribGetFunc *get, IattribSetFunc *set, const char **default_value, const char **system_default, int *flags)
-
-
-
-(defparameter *globally-readonly-attributes*
-  '(:wid :hwnd :xwindow :xdisplay
-    :modal :activewindow :maximized :minimized :mdiactive :mdinext
-    :drawdriver))
-
-(defparameter *libraries*
-  (list (list :initializer 'iup:open)
-	(list :initializer 'iup-controls:open)
-	(list :initializer 'iup-glcontrols:open)
-	(list :initializer 'iup-gl:open)
-	(list :initializer 'iup-scintilla:open)
-	(list :initializer 'iup-plot:open)
-	(list :initializer 'iup-mglplot:open)
-	#+windows (list :initializer 'iup-olecontrol:open)
-	(list :initializer 'iup-web:open :classname-excludes '("iupolecontrol"))
-	#+windows (list :initializer 'iup-olecontrol:open)))
-
-(loop with base-classnames = (iup:with-iup () (iup:all-classes))
-      for module-metadata in *libraries*
-      for initializer = (getf module-metadata :initializer)
-      for classes = (iup:with-iup () (funcall initializer) (iup:all-classes))
-      for classname-excludes = (getf module-metadata :classname-excludes)
-      for difference = (remove-if #'(lambda (classname)
-				      (find classname classname-excludes :test #'string=))
-				  (if (eq initializer 'iup:open)
-				      base-classnames
-				      (set-difference classes base-classnames :test #'string=)))
-      collect (list* :initializer initializer :classnames difference))
-
-(defparameter *classname-readonly-attributes-alist*
-  '(("filedlg" (:fileexist :status :value :multivaluecount))
-    ("colordlg" (:status))
-    ("fontdg" (:status))
-    ("fill" (:expand))
-    ("gridbox" (:numcol :numlin))
-    ("flatframe" (:decoration :decoroffset :decorsize))
-    ("tabs" (:count))
-    ("flattabs" (:count :hasfocus :buttonexpandpos))
-    ("flatscrollbox" (:scrollbar))
-    ("animatedlabel" (:running :framecount))
-    ("flatbutton" (:hasfocus :highlighted :pressed :radio))
-    ("dropbutton" (:hasfocus :highlighted :pressed))
-    ("calendar" (:today))
-    ("colorbar" (:count))
-    ("datepick" (:today))
-    ("list" (:count))
-    ("text" (:count :linecount :linevalue))
-    ("toggle" (:radio))
-    ("flattoggle" (:hasfocus :highlighted :pressed :radio))
-    ("tree" (:count :rootcount))
-    ("cells" (:canvas :first_col :first_line :image_canvas))
-    ("matrix" (:editing :edittext :editvalue :editcell))
-    ("matrixex" (:editing :edittext :editvalue :editcell :lasterror))
-    ("matrixlist" (:colorcol :imagecol :labelcol))
-    ;; TODO ...
-    ))
+(defun classes-metadata ()
+  (loop for classname in (iup:all-classes)
+	collect
+	(list :classname
+	      classname
+	      :attributes 	      
+	      (class-metadata classname))))
 
 (defparameter *static-metadata*
-  (list (list :initializer 'iup:open
-	      :child-p '("submenu" "spinbox" "radio" "backgroundbox" "scrollbox" "flatscrollbox"
-			 "detachbox" "expander" "sbox")
-	      :children-p '("menu" "cbox" "gridbox" "hbox" "vbox" "zbox" "normalizer" "frame"
-			    "flatframe" "tabs" "flattabs" "split")
-	      :override-p '("image" "imagergb" "imagergba")
-	      :vanity-alist '(("gridbox" . "grid-box")
-			      ("flatframe" . "flat-frame")
-			      ("flattabs" . "flat-tabs")
-			      ("backgroundbox" . "background-box")
-			      ("scrollbox" . "scroll-box")
-			      ("flatscrollbox" . "flat-scroll-box")
-			      ("detachbox" . "detach-box")
-			      ("animatedlabel" . "animated-label")
-			      ("flatbutton" . "flat-button")
-			      ("dropbutton" . "drop-button")
-			      ("colorbrowser" . "color-browser")
-			      ("datepick" . "date-pick")
-			      ("flatlabel" . "flat-label")
-			      ("flatseparator" . "flat-separator")
-			      ("progressbar" . "progress-bar")
-			      ("flattoggle" . "flat-toggle")
-			      ("multiline" . "multi-line")
-			      ("submenu" . "sub-menu")
-			      ("colordlg" . "color-dialog")
-			      ("filedlg" . "file-dialog")
-			      ("fontdlg" . "font-dialog")
-			      ("messagedlg" . "message-dialog")
-			      ("progressdlg" . "progress-dialog")
-			      ("parambox" . "param-box")))
-	(list :initializer 'iup-controls:open
-	      :vanity-alist '(("matrixex" . "matrix-ex")
-			      ("matrixlist" . "matrix-list")))
-	(list :initializer 'iup-gl:open
-	      :child-p '("glbackgroundbox")
-	      :vanity-alist '(("glcanvas" . "canvas")
-			      ("glsubcanvas" . "sub-canvas")))
-	(list :initializer 'iup-glcontrols:open
-	      :child-p '("glexpander" "glframe" "glscrollbox" "glsizebox")
-	      :children-p '("glcanvasbox")
-	      :vanity-alist '(("glcanvasbox" . "canvas-box")
-			      ("glsubcanvas" . "sub-canvas")
-			      ("glprogressbar" . "progress-bar")
-			      ("glscrollbox" . "scroll-box")
-			      ("glsizebox" . "size-box")
-			      ("glval" . "val")
-			      ("gltoggle" . "toggle")	
-			      ("gltext" . "text")
-			      ("gllink" . "link")	
-			      ("glframe" . "frame")
-			      ("glexpander" . "expander")	
-			      ("glbutton" . "button")	
-			      ("gllabel" . "label")
-			      ("glseparator" . "separator")
-			      ("glbackgroundbox" . "background-box")))
-	(list :initializer 'iup-plot:open)
-	(list :initializer 'iup-mglplot:open
-	      :vanity-alist '(("mglplot" . "plot")
-			      ("mgllabel" . "label")))
-	#+windows (list :initializer 'iup-olecontrol:open)
-	(list :initializer 'iup-scintilla:open
-	      :vanity-alist '(("scintilladlg" . "scintilla-dialog")))
-	(list :initializer 'iup-web:open
-	      :classname-excludes '("olecontrol")
-	      :vality-alist '(("webbrowser" . "web-browser"))))
+  '((:initializer iup:open
+     :child-p ("submenu" "spinbox" "radio" "backgroundbox" "scrollbox" "flatscrollbox"
+	       "detachbox" "expander" "sbox")
+     :children-p ("menu" "cbox" "gridbox" "hbox" "vbox" "zbox" "normalizer" "frame"
+		  "flatframe" "tabs" "flattabs" "split")
+     :override-p ("image" "imagergb" "imagergba")
+     :vanity-alist (("gridbox" . "grid-box")
+		    ("flatframe" . "flat-frame")
+		    ("flattabs" . "flat-tabs")
+		    ("backgroundbox" . "background-box")
+		    ("scrollbox" . "scroll-box")
+		    ("flatscrollbox" . "flat-scroll-box")
+		    ("detachbox" . "detach-box")
+		    ("animatedlabel" . "animated-label")
+		    ("flatbutton" . "flat-button")
+		    ("dropbutton" . "drop-button")
+		    ("colorbrowser" . "color-browser")
+		    ("datepick" . "date-pick")
+		    ("flatlabel" . "flat-label")
+		    ("flatseparator" . "flat-separator")
+		    ("progressbar" . "progress-bar")
+		    ("flattoggle" . "flat-toggle")
+		    ("multiline" . "multi-line")
+		    ("submenu" . "sub-menu")
+		    ("colordlg" . "color-dialog")
+		    ("filedlg" . "file-dialog")
+		    ("fontdlg" . "font-dialog")
+		    ("messagedlg" . "message-dialog")
+		    ("progressdlg" . "progress-dialog")
+		    ("parambox" . "param-box")))
+    (:initializer iup-controls:open
+     :vanity-alist (("matrixex" . "matrix-ex")
+		    ("matrixlist" . "matrix-list")))
+    (:initializer iup-gl:open
+     :child-p ("glbackgroundbox")
+     :vanity-alist (("glcanvas" . "canvas")
+		    ("glsubcanvas" . "sub-canvas")))
+    (:initializer iup-glcontrols:open
+     :child-p ("glexpander" "glframe" "glscrollbox" "glsizebox")
+     :children-p ("glcanvasbox")
+     :vanity-alist (("glcanvasbox" . "canvas-box")
+		    ("glsubcanvas" . "sub-canvas")
+		    ("glprogressbar" . "progress-bar")
+		    ("glscrollbox" . "scroll-box")
+		    ("glsizebox" . "size-box")
+		    ("glval" . "val")
+		    ("gltoggle" . "toggle")	
+		    ("gltext" . "text")
+		    ("gllink" . "link")	
+		    ("glframe" . "frame")
+		    ("glexpander" . "expander")	
+		    ("glbutton" . "button")	
+		    ("gllabel" . "label")
+		    ("glseparator" . "separator")
+		    ("glbackgroundbox" . "background-box")))
+    (:initializer iup-plot:open)
+    (:initializer iup-mglplot:open
+     :vanity-alist (("mglplot" . "plot")
+		    ("mgllabel" . "label")))
+    #+windows (list :initializer 'iup-olecontrol:open)
+    (:initializer iup-scintilla:open
+     :vanity-alist (("scintilladlg" . "scintilla-dialog")))
+    (:initializer iup-web:open
+     :classname-excludes '("olecontrol")
+     :vality-alist (("webbrowser" . "web-browser"))))
   "Not everything about the IUP APIs can be extracted by
 introspection. This describes the static metadata that is augmented
-with IUP metadata upon introspection.
-
-VANITY-ALIST mapping from IUP classnames to nicer Lisp name
-INITIALIZER package qualified symbol for computing export package and initialization function
-CLASSNAME-EXCLUDES IUP classnames to exclude
-CHILDREN-P the IUP class can contain children
-CHILD-P the IUP class can contain a child
-OVERRIDE don't create this class automatically")
+with IUP metadata upon introspection.")
 
 (defun create-classesdb ()
   "Create a printable representaion of IUP metadata containing enough
@@ -202,6 +146,37 @@ information to create the Lisp API at compilation time."
 					      :callbacks (sort-keywords (iup:class-callbacks classname))))))
 	    into result
 	  finally (return (list* :platform (platform) :metadata result)))))
+
+(iup:with-iup ()
+  (print (classes-metadata)))
+
+(defparameter *libraries*
+  (list (list :initializer 'iup:open)
+	(list :initializer 'iup-controls:open)
+	(list :initializer 'iup-glcontrols:open)
+	(list :initializer 'iup-gl:open)
+	(list :initializer 'iup-scintilla:open)
+	(list :initializer 'iup-plot:open)
+	(list :initializer 'iup-mglplot:open)
+	#+windows (list :initializer 'iup-olecontrol:open)
+	(list :initializer 'iup-web:open :classname-excludes '("iupolecontrol"))
+	#+windows (list :initializer 'iup-olecontrol:open)))
+
+(loop with base-classnames = (iup:with-iup () (iup:all-classes))
+      for module-metadata in *libraries*
+      for initializer = (getf module-metadata :initializer)
+      for classes = (iup:with-iup () (funcall initializer) (iup:all-classes))
+      for classname-excludes = (getf module-metadata :classname-excludes)
+      for difference = (remove-if #'(lambda (classname)
+				      (find classname classname-excludes :test #'string=))
+				  (if (eq initializer 'iup:open)
+				      base-classnames
+				      (set-difference classes base-classnames :test #'string=)))
+      collect (list* :initializer initializer :classnames difference))
+
+
+
+
 
 (defun classesdb-pathname ()
   (asdf:system-relative-pathname "iup" "classesdb" :type "lisp-sexp"))
