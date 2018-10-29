2b04710cf19597dba15b999c95eb6e5c9b2fc6bb
Matthew Kennedy <burnsidemk@gmail.com>
2018-10-29 14:29:35 -0500

    cleanup web

--- web/web.lisp
+++ web/web.lisp
@@ -1,3 +1,11 @@
+(defpackage #:iup-web
+  (:use #:common-lisp)
+  (:export #:open
+	   #:web-browser)
+  (:import-from #:iup-utils
+		#:alias)
+  (:shadow #:open))
+
 (in-package #:iup-web)
 
 (alias 'open #'iup-web-cffi::%iup-web-browser-open)
