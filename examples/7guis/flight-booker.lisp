2f75f0cabce59db9ac303d2978ddcb434c699e42
Matthew Kennedy <burnsidemk@gmail.com>
2018-11-07 20:28:49 -0600

    add excuses

--- examples/7guis/flight-booker.lisp
+++ examples/7guis/flight-booker.lisp
@@ -4,27 +4,29 @@
 
 (in-package #:iup-examples.flight-booker)
 
+;; FIXME add in other actions
+
 (defun direction-action-cb (self text item state)
+  ;; FIXME logic broken
   (declare (ignore text))
   (let ((start-date (iup:get-dialog-child self "STARTDATE"))
 	(end-date (iup:get-dialog-child self "ENDDATE"))
 	(book-button (iup:get-dialog-child self "BOOK")))
     (when (zerop state)
       (return-from direction-action-cb iup:+default+))
-    (if (= item 1)
-	(setf (iup:attribute end-date :active) "NO")
-	(setf (iup:attribute end-date :active) "YES"))
-    (when (= item 2)
-      (let ((start (iup:attribute start-date :value))
-	    (end (iup:attribute end-date :value)))
-	(setf (iup:attribute book-button :active) "YES")))
+    (case item
+      (1 (setf (iup:attribute end-date :active) "NO"
+	       (iup:attribute end-date :active) "YES"))
+      (2 (let ((start (iup:attribute start-date :value))
+	       (end (iup:attribute end-date :value)))
+	   (setf (iup:attribute book-button :active) (if (string<= start end) "YES" "NO")))))
     iup:+default+))
 
 (defun flight-booker ()
   (iup:with-iup ()
     (let* ((direction (iup:list :expand "HORIZONTAL" :dropdown "YES" :action 'direction-action-cb))
-	   (start-date (iup:date-pick :expand "HORIZONTAL" :name "STARTDATE" :zeropreced "YES"))
-	   (end-date (iup:date-pick :expand "HORIZONTAL" :name "ENDDATE" :zeropreced "YES"))
+	   (start-date (iup:date-pick :expand "HORIZONTAL" :name "STARTDATE"))
+	   (end-date (iup:date-pick :expand "HORIZONTAL" :name "ENDDATE"))
 	   (book-button (iup:button :title "Book" :name "BOOK" :expand "HORIZONTAL"))
 	   (vbox (iup:vbox (list direction start-date end-date book-button)
 			   :gap 10
@@ -36,7 +38,7 @@
 	    (iup:attribute start-date :active) "YES"
 	    (iup:attribute start-date :other) end-date
 	    (iup:attribute end-date :active) "NO"
-	    (iup:attribute start-date :active) start-date)
+	    (iup:attribute end-date :other) start-date)
       (iup:show dialog)
       (iup:main-loop))))
 
