99994f02724ab540af2bcb56db63b1ecc5b17056
Matthew Kennedy <burnsidemk@gmail.com>
2018-11-26 13:41:50 -0600

    example

--- examples/canvas.lisp
+++ examples/canvas.lisp
@@ -8,34 +8,40 @@
 (defparameter *levels* 0)
 (defparameter *canvas* nil)
 
-(defun sierpinski-spin (handle pos)
+(defun canvas-spin (handle pos)
   (declare (ignore pos))
   (setf *levels* (iup:attribute handle "VALUE" 'integer))
   iup:+default+)
 
-(defun sierpinski-redraw (handle x y)
+(defun canvas-redraw (handle x y)
   (declare (ignore handle x y))
   (cd:activate *canvas*)
   (cd:clear *canvas*)
-  (setf (cd:foreground *canvas*) cd:+red+)
+  (setf (cd:foreground *canvas*) cd:+black+)
   (cd-examples.sierpinski:sierpinski *canvas* *levels*)
+  (cd:flush *canvas*)
   iup:+default+)
 
-(defun sierpinski-map (handle)
-  (setf *canvas* (cd:create-canvas (iup-cd:context-iup) handle))
+(defun canvas-map (handle)
+  (setf *canvas* (cd:create-canvas (iup-cd:context-iup-dbuffer) handle))
+  iup:+default+)
+
+(defun canvas-unmap (handle)
+  (declare (ignore handle))
+  (cd:kill *canvas*)
   iup:+default+)
 
 (defun sierpinski ()
   (iup:with-iup ()
     (let* ((canvas (iup:canvas :rastersize "200x200"))
-	   (spin (iup:text :spin "YES" :spinmin 0 :spinmax 5))
+	   (spin (iup:text :spin "YES" :spinmin 0 :spinmax 6))
 	   (vbox (iup:vbox (list canvas spin) :alignment "ACENTER"))
 	   (dialog (iup:dialog vbox)))
-      (setf (iup:callback canvas :map_cb) 'sierpinski-map
-	    (iup:callback canvas :action) 'sierpinski-redraw
-	    (iup:callback spin :spin_cb) 'sierpinski-spin)
-      (cd-context-plus:initialize)
-      (cd:use-context-plus t)
+      (setf (iup:callback canvas :map_cb) 'canvas-map
+	    (iup:callback canvas :unmap_cb) 'canvas-unmap
+	    (iup:callback canvas :action) 'canvas-redraw
+	    (iup:callback spin :spin_cb) 'canvas-spin
+	    *levels* 0)
       (iup:show-xy dialog iup:+center+ iup:+center+)
       (iup:main-loop))))
 
@@ -43,7 +49,3 @@
 (sb-int:with-float-traps-masked
     (:divide-by-zero :invalid)
   (sierpinski))
-
-
-
-
