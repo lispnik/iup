974d58ba37375b86a3bbfc5ec95158bcd1a60ee5
Matthew Kennedy <burnsidemk@gmail.com>
2019-04-28 00:51:37 -0500

    Add a note about companion project tecgraf-libs

--- examples/callback.lisp
+++ examples/callback.lisp
@@ -7,21 +7,21 @@
 (defun callback ()
     (iup:with-iup ()
       (let* ((button
-	       (iup:button :title "&OK"
-			   :expand "YES"
-			   :tip "Exit button"
-			   :action (lambda (handle)
-				     (iup:message "Message"
-						  (lisp-implementation-version))
-				     iup:+close+)))
-	     (label (iup:label :title (lisp-implementation-type)))
-	     (vbox (iup:vbox (list label button)
-			     :gap "10"
-			     :margin "10x10"
-			     :alignment :acenter))
-	     (dialog (iup:dialog vbox :title "Hello, World!")))
-	(iup:show dialog)
-	(iup:main-loop))))
+               (iup:button :title "&OK"
+                           :expand "YES"
+                           :tip "Exit button"
+                           :action (lambda (handle)
+                                     (iup:message "Message"
+                                                  (lisp-implementation-version))
+                                     iup:+close+)))
+             (label (iup:label :title (lisp-implementation-type)))
+             (vbox (iup:vbox (list label button)
+                             :gap "10"
+                             :margin "10x10"
+                             :alignment :acenter))
+             (dialog (iup:dialog vbox :title "Hello, World!")))
+        (iup:show dialog)
+        (iup:main-loop))))
 
 #-sbcl (hello)
 
