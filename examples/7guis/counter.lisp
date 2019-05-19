a0bb0ea89222d6de189a4901764018a90f09cd43
Matthew Kennedy <burnsidemk@gmail.com>
2019-05-18 22:37:48 -0500

    Rework and simplify attributes

--- examples/7guis/counter.lisp
+++ examples/7guis/counter.lisp
@@ -1,3 +1,6 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload "iup"))
+
 (defpackage #:iup-examples.counter
   (:use #:common-lisp)
   (:export #:counter))
@@ -6,19 +9,23 @@
 
 (defun counter ()
   (iup:with-iup ()
-    (let* ((count (iup:text :readonly "YES" :value 0 :size 60))
+    (let* ((count (iup:text :readonly :yes :value 0 :size 60))
 	   (button (iup:button :title "Count"
 			       :size 60
 			       :action #'(lambda (handle)
 					   (declare (ignore handle))
 					   (setf (iup:attribute count :value)
-						 (1+ (iup:attribute count :value 'integer))))))
+						 (1+ (iup:attribute count :value 'number)))
+					   iup:+default+)))
 	   (hbox (iup:hbox (list count button) :gap 10 :margin "10x10"))
 	   (dialog (iup:dialog hbox :title "Counter")))
       (iup:show dialog)
       (iup:main-loop))))
 
-#+nil
+#+sbcl
 (sb-int:with-float-traps-masked
     (:divide-by-zero :invalid)
   (counter))
+
+#-sbcl
+(counter)
