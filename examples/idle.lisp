ca3532e4c921abf8595916fd2a6658ce7cd5afc1
Matthew Kennedy <burnsidemk@gmail.com>
2019-07-04 13:29:55 -0700

    Add do-nothing option for the idle loop test for overhead measurement

--- examples/idle.lisp
+++ examples/idle.lisp
@@ -17,8 +17,13 @@
                                :alignment :acenter))
            (start-button (iup:button :title "&Start" :expand :horizontal))
            (stop-button (iup:button :title "S&top" :expand :horizontal))
+           (do-nothing nil)
+           (do-nothing-toggle (iup:toggle :title "Do nothing"
+                                          :action (lambda (handle state)
+                                                    (setf do-nothing (not do-nothing))
+                                                    iup:+default+)))
            (vbox (iup:vbox (list counter
-                                 (iup:hbox (list start-button stop-button)
+                                 (iup:hbox (list start-button stop-button do-nothing-toggle)
                                            :cgap 5))
                            :margin "5x5"))
            (dialog (iup:dialog vbox
@@ -28,8 +33,9 @@
             (lambda (handle)
               (setf (iup:idle-action)
                     (lambda ()
-                      (setf (iup:attribute counter :title)
-                            (1+ (iup:attribute counter :title 'number)))
+                      (unless do-nothing
+                        (setf (iup:attribute counter :title)
+                              (1+ (iup:attribute counter :title 'number))))
                       iup:+default+))
               iup:+default+))
       (setf (iup:callback stop-button :action)
