49c58ca5bff937ce047d258ea58463ff67ac9163
Matthew Kennedy <burnsidemk@gmail.com>
2020-07-18 11:30:14 -0500

    Experimenting...

--- examples/incomplete/threads.lisp
+++ examples/incomplete/threads.lisp
@@ -1,20 +1,15 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (ql:quickload '("iup" "bordeaux-threads" "lparallel")))
 
-(defpackage #:iup-examples.threads
-  (:use #:common-lisp)
-  (:export #:threads))
+(in-package #:iup)
 
-(in-package #:iup-examples.threads)
-
-(defvar *post-message-handler*)
+(defvar *post-message-handler* nil)
 
 (defvar *post-message-queue* (lparallel.queue:make-queue))
 
 (defun post-message-callback (&rest args)
-  (format t "postmessage_cb args: ~a~%" args)
-  (terpri)
-  (loop for message = (lparallel.queue:try-pop-queue *post-message-queue* :timeout 5)
+  (declare (ignore args))
+  (loop for message = (lparallel.queue:try-pop-queue *post-message-queue*)
         while message
         do (funcall message))
     iup:+default+)
@@ -32,22 +27,29 @@
 ;;     (:divide-by-zero :invalid)
 ;;   ())
 
+(defun start ()
+  (bt:make-thread #'iup-loop :name "iup-main-loop"))
 
-#+nil (iup:exit-loop)
-#+nil (bt:make-thread #'iup-loop :name "iup-main-loop")
+(defun stop ()
+  (call-with-main-loop-asynchronous #'iup:exit-loop)
+  (setf *post-message-queue* nil))
 
-#+nil
-(defun call-with-iup-main-loop (func)
+(defun call-with-main-loop-asynchronous (func)
   (lparallel.queue:push-queue func *post-message-queue*)
   (iup-cffi::%iup-post-message
-   *post-message-handler*
-   (cffi:null-pointer)
-   42
-   pi
-   (cffi:null-pointer)))
+   *post-message-handler* (cffi:null-pointer) 42 pi (cffi:null-pointer)))
 
+#+nil
 (progn
   (loop repeat 100 for i from 0 do
-    (call-with-iup-main-loop
+    (call-with-main-loop-asynchronous
      (lambda ()
        (format t "called from UI thread: ~a~%" i)))))
+
+#+nil
+(call-with-main-loop-asynchronous
+ (lambda ()
+   (let* ((text (iup:text :expand :yes :multiline :yes))
+          (vbox (iup:vbox (list text)))
+          (dialog (iup:dialog vbox :size "QUARTERxQUARTER")))
+     (iup:show dialog))))
