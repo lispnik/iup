1782e6ce4eae179a24661aa4c3db50b1bf3d1192
Matthew Kennedy <burnsidemk@gmail.com>
2020-07-19 17:30:52 -0500

    Experimenting

--- examples/incomplete/threads.lisp
+++ examples/incomplete/threads.lisp
@@ -1,14 +1,14 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload '("iup" "bordeaux-threads" "lparallel")))
+  (ql:quickload '("iup" "bordeaux-threads" "lparallel" "log4cl")))
 
 (in-package #:iup)
 
 (defvar *post-message-handler* nil)
 
-(defvar *post-message-queue* (lparallel.queue:make-queue))
+(defvar *post-message-queue* nil)
 
 (defun post-message-callback (&rest args)
-  (declare (ignore args))
+  (log:info "post-message-callback received args ~a" args)
   (loop for message = (lparallel.queue:try-pop-queue *post-message-queue*)
         while message
         do (funcall message))
@@ -18,36 +18,34 @@
   (iup:with-iup ()
     (setf *post-message-handler* (iup:user :postmessage_cb 'post-message-callback))
     (setf (iup:global :lockloop) :yes)
-    (iup:main-loop)))
+    (main-loop)))
 
-;; #-sbcl (iup-loop)
-
-;; #+sbcl
-;; (sb-int:with-float-traps-masked
-;;     (:divide-by-zero :invalid)
-;;   ())
-
-(defun start ()
-  (bt:make-thread #'iup-loop :name "iup-main-loop"))
-
-(defun stop ()
-  (call-with-main-loop-asynchronous #'iup:exit-loop)
-  (setf *post-message-queue* nil))
-
-(defun call-with-main-loop-asynchronous (func)
-  (lparallel.queue:push-queue func *post-message-queue*)
+(defun post-message ()
   (iup-cffi::%iup-post-message
    *post-message-handler* (cffi:null-pointer) 42 pi (cffi:null-pointer)))
 
-#+nil
-(progn
-  (loop repeat 100 for i from 0 do
-    (call-with-main-loop-asynchronous
-     (lambda ()
-       (format t "called from UI thread: ~a~%" i)))))
+(defun call-with-main-loop (func)
+  (lparallel.queue:push-queue func *post-message-queue*)
+  (post-message))
+
+(defun start ()
+  (when *post-message-handler* (exit-loop))
+  (setf *post-message-handler* nil
+        *post-message-queue* (lparallel.queue:make-queue))
+  (bt:make-thread
+   (lambda ()
+     #+sbcl (sb-int:with-float-traps-masked
+                (:divide-by-zero :invalid)
+              (iup-loop))
+     #-sbcl (iup-loop))
+   :name "iup-main-loop"))
+
+(defun stop ()
+  (call-with-main-loop #'iup:exit-loop)
+  (setf *post-message-queue* nil))
 
 #+nil
-(call-with-main-loop-asynchronous
+(call-with-main-loop
  (lambda ()
    (let* ((text (iup:text :expand :yes :multiline :yes))
           (vbox (iup:vbox (list text)))
